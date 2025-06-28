4c6980e08f63e87023338235fbca733d9d9e21f9
Nguyễn Quốc Huy <nqhuy204@clc.fitus.edu.vn>
2025-06-28 15:41:13 +0700

    Test conflict

--- intro.cpp
+++ intro.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main() {
     cout << "Hello world" << endl;
+    cout << "This is a C++ program using the C++17 standard." << endl;
+    cout << "The C++17 standard introduces several new features and improvements." << endl;
     return 0;
 }
